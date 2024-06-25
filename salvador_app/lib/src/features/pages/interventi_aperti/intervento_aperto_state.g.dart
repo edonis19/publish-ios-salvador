00ad19a4292d750e0460a48f44c50d8b558ccf1b
Assistenza iColdo <assistenza@icoldo.it>
2024-06-25 15:30:09 +0200

    publish ios

---
+++ salvador_app/lib/src/features/pages/interventi_aperti/intervento_aperto_state.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'intervento_aperto_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$interventoApertoStateHash() =>
+    r'ddee26a94af2fd3c33bb4c7d341f3583c9740b62';
+
+/// See also [InterventoApertoState].
+@ProviderFor(InterventoApertoState)
+final interventoApertoStateProvider =
+    NotifierProvider<InterventoApertoState, Intervento>.internal(
+  InterventoApertoState.new,
+  name: r'interventoApertoStateProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$interventoApertoStateHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$InterventoApertoState = Notifier<Intervento>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
